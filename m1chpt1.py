fff3b869e5b47b1a60319b1d841f5a6a1a954dc9
fidelmawalsh <fidelma_walsh@yahoo.com>
2021-04-06 19:54:21 +0100

    Initialised my repo and addemd m1chpt1

---
+++ m1chpt1.py
@@ -0,0 +1,18 @@
+# Example 1, do not modify!
+print(8 / 2)
+# Example 2, do not modify!
+print(2**2)
+print (2+5)
+print (8*5)
+print("hello")
+# Addition, no print output
+print(10+2)
+# Create company_1
+company_1 = 'Apple'
+print(company_1)
+# Test equality
+test_company = 'apple'
+print(company_1==test_company)
+
+
+
